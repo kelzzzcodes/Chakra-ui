33d4df00ce0c2cf6a28cd2deff7709bb22eaa414
Kelechi Otugbuali <hilakechi@gmail.com>
2024-04-27 18:26:49 +0100

    handled icon color change when the link is clicked

--- src/components/Bottombar.jsx
+++ src/components/Bottombar.jsx
@@ -1,36 +1,52 @@
 import { Box, Flex, Icon, Spacer, Text } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { BottombarLinks } from '../constants'
 
 const Bottombar = () => {
+  const [clicked, setClicked] = useState(false)
+  const handleClickColor = (label) => {
+    setClicked(label)
+  }
+
   return (
     <Box
       position="fixed"
       bottom="0"
-      w='376px'
+      w="376px"
       bg="white"
       color="#797B8B"
-      display='flex'
-      justifyItems='center'
-      alignItems='center'
-      height='87px'
+      display="flex"
+      justifyItems="center"
+      alignItems="center"
+      height="87px"
       padding="1rem"
       zIndex="999"
     >
-      <Flex display='flex' justifyItems='center' align="center" justify="space-between" w='311px'>
+      <Flex
+        display="flex"
+        justifyItems="center"
+        align="center"
+        justify="space-between"
+        w="311px"
+      >
         {BottombarLinks.map((items) => (
           <Link
+            onClick={() => handleClickColor(items.label)}
             key={items.label}
-            href={items.path}
+            to={items.path}
             display="flex"
             flexDirection="column"
             alignItems="center"
             justifyContent="center"
             textAlign="center"
           >
-            <Icon as={items.icon} boxSize='23.5px' />
-            <Text fontSize='xs'>{items.label}</Text>
+            <span
+              className={`${clicked === items.label ? 'text-green-400' : ''}`}
+            >
+              <Icon as={items.icon} boxSize="23.5px" />
+            </span>
+            <Text fontSize="xs">{items.label}</Text>
           </Link>
         ))}
       </Flex>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,85 +1,79 @@
-import { MdOutlineHome } from "react-icons/md";
-import { TbFileHorizontal,TbPigMoney } from "react-icons/tb";
-import { RiChatSmile2Line } from "react-icons/ri";
-import { RiBarChartFill } from "react-icons/ri";
-import { IoPersonOutline,IoRestaurantOutline } from "react-icons/io5";
-
+import { MdOutlineHome } from 'react-icons/md'
+import { TbFileHorizontal, TbPigMoney } from 'react-icons/tb'
+import { RiChatSmile2Line } from 'react-icons/ri'
+import { RiBarChartFill } from 'react-icons/ri'
+import { IoPersonOutline, IoRestaurantOutline } from 'react-icons/io5'
 
 export const BottombarLinks = [
-    {
+  {
     path: '/',
     icon: MdOutlineHome,
     label: 'Home',
-
   },
   {
-    path: '/reports',
-    icon:TbFileHorizontal,
+    path: '/',
+    icon: TbFileHorizontal,
     label: 'Reports',
   },
   {
-    path: '/chat',
+    path: '/',
     icon: RiChatSmile2Line,
     label: 'Chat',
   },
   {
-    path: '/budget',
+    path: '/',
     icon: RiBarChartFill,
     label: 'Budget',
   },
-    {
-    path: '/profile',
+  {
+    path: '/',
     icon: IoPersonOutline,
     label: 'Profile',
   },
 ]
 
-export const TabsData=[
+export const TabsData = [
   {
-    value: '20' ,
+    value: '20',
     rating: '40%',
-    expenditure:'₦50,000',
-    income: '₦120,000'
+    expenditure: '₦24,000',
+    income: '₦60,000',
   },
-    {
-    value: '70' ,
-    rating: '50%',
-    expenditure:'₦12,000',
-    income: '₦24,000'
+  {
+    value: '70',
+    rating: '49%',
+    expenditure: '₦50,000',
+    income: '₦120,000',
   },
 ]
 
 export const CategoryData = [
-
   {
     label: 'Food and Drinks',
     icon: IoRestaurantOutline,
-    rating: '40%' ,
+    rating: '40%',
     expenditure: '₦20,000',
-    income: '₦42,000'
+    income: '₦42,000',
   },
-   {
+  {
     label: 'Savings',
     icon: TbPigMoney,
-    rating: '10%' ,
+    rating: '10%',
     expenditure: '₦10,000',
-    income: '₦24,000'
+    income: '₦24,000',
   },
   {
     label: 'events',
     icon: IoRestaurantOutline,
-    rating: '20%' ,
+    rating: '20%',
     expenditure: '₦20,000',
-    income: '₦42,000'
+    income: '₦42,000',
   },
   {
     label: 'School fees',
     icon: TbPigMoney,
-    rating: '10%' ,
+    rating: '10%',
     expenditure: '₦10,000',
-    income: '₦24,000'
+    income: '₦24,000',
   },
-
-
-
 ]
